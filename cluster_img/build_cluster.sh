01cd6c7628241346c278dcb98bb427effd7684f0
TPOTD <kurdymov01@gmail.com>
2022-09-04 22:33:19 +0300

    add docker compose

--- cluster_img/build_cluster.sh
+++ cluster_img/build_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t cluster .
+docker build -t cluster ./cluster_img
 docker tag cluster 65.108.82.162:8090/cluster
 docker push 65.108.82.162:8090/cluster
--- logic_scripts/create_services.sh
+++ logic_scripts/create_services.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 docker service create --replicas 1 --name serving -p 5555:5555 --mount type=bind,src=/opt/models,dst=/models 65.108.82.162:8090/serving
-
 docker service create --replicas 1  --name api -p 5050:5050 --mount type=bind,src=/opt/clusters/,dst=/clusters --host host.docker.internal:host-gateway 65.108.82.162:8090/api
-
-docker service create --replicas 1 --name cluster_service_0 -p 5010:5005  --mount type=bind,src=/opt/clusters/,dst=/clusters -e CLUSTER=0 65.108.82.162:8090/cluster
-
-docker service create --replicas 1 --name cluster_service_1 -p 5011:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters -e CLUSTER=1 65.108.82.162:8090/cluster
-
+docker service create --replicas 1 --name cluster_service_0 -p 5010:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters -e CLUSTER=0 65.108.82.162:8090/cluster
+docker service create --replicas 1 --name cluster_service_1 -p 5011:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters --constraint node.hostname==kcloud-production-user-154-vm-540 -e CLUSTER=1 65.108.82.162:8090/cluster
 docker service create --replicas 1 --name cluster_service_2 -p 5012:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters -e CLUSTER=2 65.108.82.162:8090/cluster
-
-docker service create --replicas 1 --name cluster_service_3 -p 5013:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters --constraint node.hostname==kcloud-production-user-107-vm-306 -e CLUSTER=3 65.108.82.162:8090/cluster
+docker service create --replicas 1 --name cluster_service_3 -p 5013:5005 --mount type=bind,src=/opt/clusters/,dst=/clusters  -e CLUSTER=3 65.108.82.162:8090/cluster
