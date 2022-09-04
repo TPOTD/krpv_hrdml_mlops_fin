a4be6365914c457d828bc815b9a36a711d3548e6
TPOTD <kurdymov01@gmail.com>
2022-09-05 02:15:20 +0300

    fixed bugs

--- api_img/app.py
+++ api_img/app.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 def process_data():
     query = request.get_data().decode('utf-8')
     query_emb = requests.post('http://host.docker.internal:5555/process', query).json()
-    query_emb = np.array(json.loads(query)['embendings'], dtype='float32')[0]
+    query_emb = np.array(query_emb['embendings'], dtype='float32')[0]
     
     ab = np.sum(query_emb*centers, axis=1)
     norm_a = np.linalg.norm(query_emb)
     norm_b = np.linalg.norm(centers, axis = 1)
     cos_sims = ab / (norm_a * norm_b)
     clstr = np.argmin(cos_sims)
-    
+    clstr = 2 if clstr == 4 else clstr
     res = requests.post(f'http://host.docker.internal:501{clstr}/find_match', json={'embed':query_emb.tolist()}).json()
     return jsonify(res)
