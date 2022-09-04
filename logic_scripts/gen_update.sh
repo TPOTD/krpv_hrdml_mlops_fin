c1fe23817dbcd5c1b5b497be952eb8983be63d5a
TPOTD <kurdymov01@gmail.com>
2022-09-04 23:38:04 +0300

    update of update

--- logic_scripts/gen_update.sh
+++ logic_scripts/gen_update.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 # args:
 # prev_gen
 # new_gen
 # cluster 
-
-
 for host in kc1 kc2; do
     # убираем всё по текущему кластеру из Bind Mounted папок
     ssh $host "mv /opt/clusters/clust$3 /storage/dgs/dg$1/clust$3"
@@ -15,5 +12,5 @@ for host in kc1 kc2; do
     ssh $host "python3 ./logic_scripts/centers_update.py --cluster $3 --new_gen $2 "
 done
 # rolling update сервиса кластера и API gateway
-ssh kc1 "docker service update --force --update-parallelism 1 --update-delay 30s --update-failure-action=rollback --update-max-failure-ratio 0 qaserv_index_service_$3"
-ssh kc1 "docker service update --force --update-parallelism 1 --update-delay 30s --update-failure-action=rollback --update-max-failure-ratio 0 qaserv_gateway"
+ssh kc1 "docker service update --force --update-parallelism 1 --update-delay 20s --update-failure-action=rollback --update-max-failure-ratio 0 qaserv_index_service_$3"
+ssh kc1 "docker service update --force --update-parallelism 1 --update-delay 20s --update-failure-action=rollback --update-max-failure-ratio 0 qaserv_gateway"
